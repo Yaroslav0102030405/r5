28fee33136c833178fef03ba3fe7b8eef7dc348a
Yaroslav0102030405 <lukovetskyi@gmail.com>
2025-08-12 19:41:15 +0300

    filter + form validate

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,89 @@
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button/Button";
 import MainLayout from "./components/layout/MainLayout";
 import Users from "./services/users/Users";
 
+interface Color {
+  label: string;
+}
+
+interface FormData {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+}
+
+const color = [{ label: "red" }, { label: "green" }, { label: "blue" }];
+
 function App() {
+  const [fomrData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+  const [validationErrors, setValidationErrors] = useState<Partial<FormData>>(
+    {}
+  );
+  const [selectedColor, setSelectedColor] = useState<Color[]>(color);
+  const [show, setShow] = useState<boolean>(false);
+  const [number, setNumber] = useState<number>(1);
+
   const handleClick = (data: string) => {
     alert(data);
+  };
+
+  const handleIncrement = () => {
+    setNumber((prev) => prev + 1);
+  };
+
+  const handleDecrerment = () => {
+    setNumber((prev) => prev - 1);
+  };
+
+  const handleShow = () => {
+    setShow((prev) => !prev);
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const errors: Partial<FormData> = {};
+    if (!fomrData.name.trim()) {
+      errors.name = "Ім’я є обов’язковим";
+    }
+    if (!fomrData.email.trim()) {
+      errors.email = "Email є обов’язковим";
+    }
+    if (!fomrData.phone.trim()) {
+      errors.phone = "Телефон є обов’язковим";
+    }
+    if (!fomrData.message.trim()) {
+      errors.message = "Повідомлення є обов’язковим";
+    }
+
+    setValidationErrors(errors);
+    if (Object.keys(errors).length === 0) {
+      console.log(fomrData);
+      setFormData({
+        name: "",
+        email: "",
+        phone: "",
+        message: "",
+      });
+      console.log("Форма успішно відправлена!");
+    } else {
+      console.log("Помилка валідації форми:", errors);
+    }
   };
   return (
     <>
@@ -13,7 +91,79 @@ function App() {
       <MainLayout>
         <Users />
         <Button label="Натисни на мене" onClick={handleClick} />
+        <button onClick={handleIncrement}>+</button>
+        <p>{number}</p>
+        <button onClick={handleDecrerment} disabled={number === 1}>
+          -
+        </button>
       </MainLayout>
+      <div>
+        <button onClick={handleShow}>{show ? "Сховати" : "Показати"}</button>
+
+        {show && <p>Випадающе меню</p>}
+      </div>
+      <ul>
+        {selectedColor.map((item, index) => (
+          <li key={index}>
+            <span>{item.label}</span>
+            <button
+              onClick={() =>
+                setSelectedColor((prev) => prev.filter((_, i) => i !== index))
+              }
+            >
+              Видалити
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            name="name"
+            value={fomrData.name}
+            onChange={handleChange}
+          />
+          {validationErrors.name && (
+            <span className="error">{validationErrors.name}</span>
+          )}
+        </label>
+        <label>
+          <input
+            type="email"
+            name="email"
+            value={fomrData.email}
+            onChange={handleChange}
+          />
+          {validationErrors.email && (
+            <span className="error">{validationErrors.email}</span>
+          )}
+        </label>
+        <label>
+          <input
+            type="tel"
+            name="phone"
+            value={fomrData.phone}
+            onChange={handleChange}
+          />
+          {validationErrors.phone && (
+            <span className="error">{validationErrors.phone}</span>
+          )}
+        </label>
+        <label>
+          <textarea
+            name="message"
+            rows={6}
+            value={fomrData.message}
+            onChange={handleChange}
+          ></textarea>
+          {validationErrors.message && (
+            <span className="error">{validationErrors.message}</span>
+          )}
+        </label>
+        <button type="submit">Відправити</button>
+      </form>
     </>
   );
 }
